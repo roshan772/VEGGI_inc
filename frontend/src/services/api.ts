432843fe4d0d711bb3d18a81326055eb221b7de5
roshan772 <krishroishanroshan1221@gmail.com>
2026-01-01 19:01:20 +0530

    Mongodb Atlas and vercel update

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8000/api/v1";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1";
 
 const api = axios.create({
   baseURL: API_URL,
